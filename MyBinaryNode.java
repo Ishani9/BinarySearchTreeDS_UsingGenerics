9a96b15b5d1e67e048726e494e07d86c97ab3fc6
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-10-15 12:23:36 +0530

    Welcome to BST using Linked list

---
+++ MyBinaryNode.java
@@ -0,0 +1,29 @@
+package com.bl.assignment;
+
+public class MyBinaryNode<k extends Comparable<k>> implements INode<k> {
+	k key;
+	MyBinaryNode<k> leftNode;
+	MyBinaryNode<k> rightNode;
+	
+	@Override
+	public void setKey(k key) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public k getKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setNext(INode<k> node) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public INode<k> getNext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+} 
